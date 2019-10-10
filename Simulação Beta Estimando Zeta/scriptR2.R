d7872e414222e8cee9e45e964761dfb42fe41ac5
pjoao266 <33253735+pjoao266@users.noreply.github.com>
2019-10-10 01:01:45 -0300

    Simulação Feita com n = 5

--- Simulação Beta Estimando Zeta/scriptR2.R
+++ Simulação Beta Estimando Zeta/scriptR2.R
@@ -89,45 +89,88 @@ output = stan(file = "scriptSTAN2.stan", data=data,
 print(output, pars=c("beta0","beta1","beta2","gama0","gama1","gama2","zeta[1]","theta[1]"))
 
 samp = extract(output)
-sbeta0 = samp$beta0; szeta = samp$zeta;
+sbeta0 = samp$beta0; 
+szeta = samp$zeta;
 sbeta1 = samp$beta1;
 sbeta2 = samp$beta2;
-
-
+sgama0 = samp$gama0; 
+sgama1 = samp$gama1; 
+sgama2 = samp$gama2; 
 
 # Traceplots das cadeias (sequência de amostras a posteriori)
 par(mfrow=c(1,2))
 plot(sbeta0,type="l",ylab="beta0",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
 abline(h=beta0_real,lwd=2,col="red")
+plot(sbeta1,type="l",ylab="beta1",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
+abline(h=beta1_real,lwd=2,col="red")
+plot(sbeta2,type="l",ylab="beta2",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
+abline(h=beta2_real,lwd=2,col="red")
+plot(sgama0,type="l",ylab="gama0",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
+abline(h=gama0_real,lwd=2,col="red")
+plot(sgama1,type="l",ylab="gama1",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
+abline(h=gama1_real,lwd=2,col="red")
+plot(sgama2,type="l",ylab="gama2",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
+abline(h=gama2_real,lwd=2,col="red")
 plot(szeta,type="l",ylab="zeta",xlab="iterações",cex.lab=1.5,cex.axis=1.5)
 abline(h=zeta_real,lwd=2,col="red")
 
 # Densidades a posteriori.
 par(mfrow=c(1,2))
 plot(density(sbeta0),main="Desnidade a posteriori beta0",col="red")
+plot(density(sbeta1),main="Desnidade a posteriori beta1",col="red")
+plot(density(sbeta2),main="Desnidade a posteriori beta2",col="red")
+plot(density(sgama0),main="Desnidade a posteriori gama0",col="red")
+plot(density(sgama1),main="Desnidade a posteriori gama1",col="red")
+plot(density(sgama2),main="Desnidade a posteriori gama2",col="red")
 plot(density(szeta),main="Densidade a posteriori zeta",col="red")
 
 # Posteriori versus Priori.
 par(mfrow=c(1,2))
 plot(beta0,dm,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
 lines(density(sbeta0),lwd=2,main="Desnidade a posteriori mu",col="red")
+plot(beta1,dm,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
+lines(density(sbeta1),lwd=2,main="Desnidade a posteriori mu",col="red")
+plot(beta2,dm,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
+lines(density(sbeta2),lwd=2,main="Desnidade a posteriori mu",col="red")
+plot(gama0,ds,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
+lines(density(sgama0),lwd=2,main="Desnidade a posteriori mu",col="red")
+plot(gama1,ds,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
+lines(density(sgama1),lwd=2,main="Desnidade a posteriori mu",col="red")
+plot(gama2,ds,type="l",lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,3.5))
+lines(density(sgama2),lwd=2,main="Desnidade a posteriori mu",col="red")
 plot(zeta,type="l",ds,lwd=2,col="blue",ylab="densidade",cex.lab=2,cex.axis=2,ylim=c(0,2))
 lines(density(szeta),lwd=2,main="Densidade a posteriori zeta",col="red")
 
 require(coda)
 # Estimativa intervalar (intervalo de credibilidade HPD - High Posterior Density)
 beta0_mcmc = as.mcmc( c(sbeta0) )
+beta1_mcmc = as.mcmc( c(sbeta1) )
+beta2_mcmc = as.mcmc( c(sbeta2) )
+gama0_mcmc = as.mcmc( c(sgama0) )
+gama1_mcmc = as.mcmc( c(sgama1) )
+gama2_mcmc = as.mcmc( c(sgama2) )
 zeta_mcmc = as.mcmc( c(szeta) )
 hpd1 = HPDinterval(beta0_mcmc, prob=0.95)
-hpd1[1:2]
-hpd2 = HPDinterval(zeta_mcmc, prob=0.95)
-hpd2[1:2]
+hpd2 = HPDinterval(beta1_mcmc, prob=0.95)
+hpd3 = HPDinterval(beta2_mcmc, prob=0.95)
+hpd4 = HPDinterval(gama0_mcmc, prob=0.95)
+hpd5 = HPDinterval(gama1_mcmc, prob=0.95)
+hpd6 = HPDinterval(gama2_mcmc, prob=0.95)
+hpd7 = HPDinterval(zeta_mcmc, prob=0.95)
 
 # Estimativas pontuais
 est1 = c(mean(sbeta0),median(sbeta0),var(sbeta0),sd(sbeta0),hpd1[1:2])
-est2 = c(mean(szeta),median(szeta),var(szeta),sd(szeta),hpd2[1:2])
+est2 = c(mean(sbeta1),median(sbeta1),var(sbeta1),sd(sbeta1),hpd2[1:2])
+est3 = c(mean(sbeta2),median(sbeta2),var(sbeta2),sd(sbeta2),hpd3[1:2])
+
+est4 = c(mean(sgama0),median(sgama0),var(sgama0),sd(sgama0),hpd4[1:2])
+est5 = c(mean(sgama1),median(sgama1),var(sgama1),sd(sgama1),hpd5[1:2])
+est6 = c(mean(sgama2),median(sgama2),var(sgama2),sd(sgama2),hpd6[1:2])
+
+est7 = c(mean(szeta),median(szeta),var(szeta),sd(szeta),hpd7[1:2])
+
 tab = rbind(est1,est2)
-colnames(tab) = c("Média","Mediana","Variância","Desvio_Padrão","HPD_inf","HPD_sup")
+colnames(tab) = c("M�dia","Mediana","Variância","Desvio_Padrão","HPD_inf","HPD_sup")
 rownames(tab) = c("beta0","zeta")
 tab
 
